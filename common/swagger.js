9ce497229f29131ce2a154bd7ed346dcd3a88c5f
AlfieriChou <alfierichou@gmail.com>
2018-08-08 22:06:37 +0800

    swagger path restful

--- common/swagger.js
+++ common/swagger.js
@@ -1,22 +1,44 @@
 const fs = require('fs')
-function getAllDirs(modelPath = '../model'){
+const Joi = require('joi')
+const convert = require('joi-to-json-schema')
+const j2s = require('joi-to-swagger')
+
+const getAllDirs = (modelPath = '../model') => {
   const items = fs.readdirSync(modelPath)
   // let reg = /\.\w+$/
   items.map(item => {
     // item = item.toString()
     // item = item.replace(reg, '')
     let model = require('../model/' + item)
-    let result
     for(let x in model) {
-      console.log('----->', x)
-      result = {
-        path: model[x].path,
-        method: model[x].method
+      content = {
+        tags: model[x].tags,
+        summary: model[x].summary,
+        description: model[x].description
       }
-      console.log('--------result', result)
+      if (model[x].validate.params) {
+        params = j2s(Joi.object(model[x].validate.params)).swagger
+        content.parameters = params
+      }
+      if (model[x].validate.headers) {
+        headers = j2s(Joi.object(model[x].validate.headers)).swagger
+        content.headers = headers
+      }
+      if (model[x].validate.body) {
+        body = j2s(Joi.object(model[x].validate.body)).swagger
+        content.body = body
+      }
+      content.responses = {'200': convert(model[x].output.body)}
+      // console.log('--------content', content)
+      let swaggerMethod = {}
+      swaggerMethod[(model[x].method).toString()] = content
+      let swaggerItem = {}
+      swaggerItem[(model[x].path).toString()] = swaggerMethod
+      console.log('----'+model[x].method+'---->', swaggerItem)
+      // console.log('----path----', swaggerMethod)
     }
-    console.log('------>', model)
-    console.log('------->', item)
+    // console.log('------->', model)
+    // console.log('------->', item)
   })
   return items
 }
