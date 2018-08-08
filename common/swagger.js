cdb671cb9ba6e3be8254edd34a20503e970aed02
AlfieriChou <alfierichou@gmail.com>
2018-08-08 18:35:41 +0800

    for in result

--- common/swagger.js
+++ common/swagger.js
@@ -1,11 +1,20 @@
 const fs = require('fs')
 function getAllDirs(modelPath = '../model'){
   const items = fs.readdirSync(modelPath)
-  let reg = /\.\w+$/
+  // let reg = /\.\w+$/
   items.map(item => {
-    item = item.toString()
-    item = item.replace(reg, '')
+    // item = item.toString()
+    // item = item.replace(reg, '')
     let model = require('../model/' + item)
+    let result
+    for(let x in model) {
+      console.log('----->', x)
+      result = {
+        path: model[x].path,
+        method: model[x].method
+      }
+      console.log('--------result', result)
+    }
     console.log('------>', model)
     console.log('------->', item)
   })
