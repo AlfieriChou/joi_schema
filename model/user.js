ace17f7c079e0ef340ea12a2c141014c300f5b22
AlfieriChou <alfierichou@gmail.com>
2018-08-08 22:24:10 +0800

    update model & joi-to-swagger module

--- model/user.js
+++ model/user.js
@@ -13,22 +13,43 @@ module.exports = {
     method: 'get',
     tags: ['users'],
     summary: '获取用户列表',
-    description: `获取用户列表信息，通过不同的查询条件得到不同的查询结果`,
+    description: '获取用户列表信息，通过不同的查询条件得到不同的查询结果',
     validate: {
       params: {
-        phone: Joi.string().description('手机号')
+        phone: Joi.string().description('手机号'),
+        password: Joi.string().description('密码').required()
       }
     },
     output: {
-      '200': {
-        body: Joi.object({
-          code: Joi.number().description('返回标识'),
-          message: Joi.string().description('接口描述'),
-          data: Joi.array().items(_.omit(props, ['password'])).description('返回结果')
-        }).options({
-          allowUnknown: true
-        }).description('返回用户列表')
+      body: Joi.object({
+        code: Joi.number().description('返回标识'),
+        message: Joi.string().description('接口描述'),
+        data: Joi.array().items(_.omit(props, ['password'])).description('返回结果')
+      }).options({
+        allowUnknown: true
+      }).description('返回用户列表')
+    }
+  },
+  create: {
+    path: '/users',
+    method: 'post',
+    tags: ['users'],
+    summary: '创建用户列表',
+    description: '创建用户列表信息',
+    validate: {
+      body: {
+        phone: Joi.string().description('手机号').required(),
+        password: Joi.string().description('密码').required()
       }
+    },
+    output: {
+      body: Joi.object({
+        code: Joi.number().description('返回标识'),
+        message: Joi.string().description('接口描述'),
+        data: Joi.array().items(_.omit(props, ['password'])).description('返回结果')
+      }).options({
+        allowUnknown: true
+      }).description('返回用户信息')
     }
   }
 }
