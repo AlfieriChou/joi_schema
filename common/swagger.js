d2eca664745ccc37b8b3e18fb179b5c8113f9eda
AlfieriChou <alfierichou@gmail.com>
2018-08-11 20:15:25 +0800

    add swagger path generate

--- common/swagger.js
+++ common/swagger.js
@@ -9,28 +9,26 @@ const generateSwagger = (modelPath = './model') => {
   let methods = []
   items.forEach(item => {
     let model = require('../model/' + item)
-    let schema = []
+    let schema = {}
     for(let index in model) {
       
       if (index === 'schema') {
         modelSchema = convert(model[index])
-        for (let prop in modelSchema.properties) {
-          let field = {}
-          field.name = prop
-          field.items = {
-            'type' : modelSchema.properties[prop].type 
+        schema = {
+          'User' : {
+            'type' : 'object',
+            'properties' : modelSchema.properties
           }
-          schema.push(field)
         }
       } else {
         content = {
           tags: model[index].tags,
           summary: model[index].summary,
-          description: model[index].description
+          description: model[index].description,
+          parameters : []
         }
 
         if (model[index].validate.params) {
-          content.parameters = []
           params = convert(Joi.object(model[index].validate.params))
           for (let prop in params.properties) {
             let field = {}
@@ -43,8 +41,20 @@ const generateSwagger = (modelPath = './model') => {
           }
         }
 
+        if (model[index].validate.path) {
+          params = convert(Joi.object(model[index].validate.path))
+          for (let prop in params.properties) {
+            let field = {}
+            field.name = prop
+            field.in = 'path'
+            field.description = params.properties[prop].description
+            field.type = params.properties[prop].type
+            field.required = (params.required).indexOf(prop) > -1 ? true : false
+            content.parameters.push(field)
+          }
+        }
+
         if (model[index].validate.headers) {
-          content.parameters = []
           params = convert(Joi.object(model[index].validate.headers))
           for (let prop in params.properties) {
             let field = {}
@@ -60,11 +70,10 @@ const generateSwagger = (modelPath = './model') => {
         }
 
         if (model[index].validate.body) {
-          content.parameters = []
           params = convert(Joi.object(model[index].validate.body))
           let field = {}
           field.in = 'body'
-          field.name = 'create'
+          field.name = 'body'
           field.description = model[index].description
           field.schema = {
             'type' : params.type,
@@ -77,7 +86,7 @@ const generateSwagger = (modelPath = './model') => {
         // TODO response存在Schema格式问题
         content.responses = {
           200: {
-            description: 'response success'
+            'description' : 'response success'
           }
         }
 
--- model/user.js
+++ model/user.js
@@ -18,10 +18,7 @@ module.exports = {
     summary: '获取用户列表',
     description: '获取用户列表信息，通过不同的查询条件得到不同的查询结果',
     validate: {
-      params: {
-        phone: Joi.string().description('手机号'),
-        password: Joi.string().description('密码')
-      }
+      params: _.pick(props, ['phone', 'password'])
     },
     output: {
       body: Joi.object({
@@ -44,15 +41,22 @@ module.exports = {
         phone: Joi.string().description('手机号').required(),
         password: Joi.string().description('密码').required()
       }
-    },
-    output: {
-      body: Joi.object({
-        code: Joi.number().description('返回标识'),
-        message: Joi.string().description('接口描述'),
-        data: Joi.array().items(_.omit(props, ['password'])).description('返回结果')
-      }).options({
-        allowUnknown: true
-      }).description('返回用户信息')
+    }
+  },
+  update: {
+    path: '/users/{id}',
+    method: 'put',
+    tags: ['users'],
+    summary: '更新用户信息',
+    description: '更新用户信息',
+    validate: {
+      path: {
+        id: Joi.number().integer().description('id').required()
+      },
+      body: {
+        phone: Joi.string().description('手机号').required(),
+        password: Joi.string().description('密码').required()
+      }
     }
   }
 }
